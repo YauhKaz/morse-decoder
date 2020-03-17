eef95b62082ad1fefe6d98860e317dae01e7be17
unknown <C:\Users\User\AppData\Roaming\The Bat!>
2020-03-17 10:41:40 +0300

    docs: update

--- src/index.js
+++ src/index.js
@@ -38,7 +38,34 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+   expr = expr.split('');
+   let str='';
+   for (let j=0; expr.length; j++)
+   {
+       if (expr[j]==' ') str = '**********'; 
+       else 
+       {
+       let temp='';
+    for (let i=0; i<MORSE_TABLE.length; i++)
+    {
+        if (MORSE_TABLE(key)==expr[j]) 
+        {
+            temp=key;
+            break;
+        }
+    } 
+    let tempstr='';
+    for (let k=0; k< temp.length; k++)
+    {
+        for (let ki=0; ki< 5-temp.length; ki++)
+        {
+            tempstr+='00';
+        }
+        if (temp[k]=='.') tempstr+='11';
+        if (temp[k]=='-') tempstr+='10';
+    }
+   }   
+   }
 }
 
 module.exports = {
